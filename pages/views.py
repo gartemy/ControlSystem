6740039818dea8e262990aca728116b60e175c90
ArtemGanzha <artemganzhaa@gmail.com>
2021-01-21 02:15:49 +0500

    Sending commit

--- pages/views.py
+++ pages/views.py
@@ -2,19 +2,21 @@ from django.shortcuts import render, redirect, HttpResponse
 from .forms import DocumentForm
 from .models import Document
 from .edit import edit
-import sqlite3
+import os, sqlite3
 
 def index(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         document_name = request.FILES['document'].name
-        if form.is_valid():
+        if form.is_valid() and document_name.endswith(".doc") or document_name.endswith(".docx"):
             form.save()
-            #connection = sqlite3.connect('db.sqlite3')
-            #cursor = connection.cursor()
-            #query = "SELECT document FROM pages_document WHERE document = 'documents/%s'" % (document_name) 
-            #document = cursor.execute(query) 
             edit('media/documents/' + document_name)
+            document_path = 'media/documents/' + document_name
+            FilePointer = open(document_path,"rb")
+            response = HttpResponse(FilePointer,content_type='application/msword')
+            response['Content-Disposition'] = 'attachment; filename=%s' %document_name
+            os.remove(document_path)
+            return response
             redirect('pages/index.html')
     else:
         form = DocumentForm()
